edeb4160a1c0f10605c467cd32b8cf3d998af2d8
lijiankun <1732931774@qq.com>
2025-03-16 01:16:46 +0800

    访问购物车时配置拦截器,进入登录界面

---
+++ mall-portal/src/main/java/com/jiankun/mall/config/WebConfigurer.java
@@ -0,0 +1,21 @@
+package com.jiankun.mall.config;
+
+import com.jiankun.mall.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author OfferKing
+ * @version 1.0
+ * @date 2025/3/3 14:26
+ */
+@Configuration
+public class WebConfigurer implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/page/cart/**", "/page/order/**", "/page/user/**");
+    }
+}
