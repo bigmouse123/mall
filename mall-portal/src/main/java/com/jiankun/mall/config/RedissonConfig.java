b5b20d96cfefec31ebb906ee3d6cb583369c50bf
lijiankun <1732931774@qq.com>
2025-03-19 16:28:16 +0800

    redis

---
+++ mall-portal/src/main/java/com/jiankun/mall/config/RedissonConfig.java
@@ -0,0 +1,23 @@
+package com.jiankun.mall.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author OfferKing
+ * @version 1.0
+ * @date 2025/3/19 16:15
+ */
+@Configuration
+public class RedissonConfig {
+    @Bean
+    public RedissonClient redissonClient(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://127.0.0.1:6379");
+        RedissonClient redisson = Redisson.create(config);
+        return redisson;
+    }
+}
--- mall-portal/src/main/java/com/jiankun/mall/service/impl/ProductServiceImpl.java
+++ mall-portal/src/main/java/com/jiankun/mall/service/impl/ProductServiceImpl.java
@@ -7,10 +7,13 @@ import com.jiankun.mall.pojo.Product;
 import com.jiankun.mall.pojo.query.ProductQuery;
 import com.jiankun.mall.service.IProductService;
 import com.jiankun.mall.util.PageResult;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author OfferKing
@@ -21,6 +24,10 @@ import java.util.List;
 public class ProductServiceImpl implements IProductService {
     @Autowired
     private ProductMapper productMapper;
+    @Autowired
+    private RedisTemplate redisTemplate;
+    @Autowired
+    private RedissonClient redissonClient;
 
     @Override
     public PageResult<Product> list(ProductQuery productQuery) {
@@ -33,6 +40,42 @@ public class ProductServiceImpl implements IProductService {
 
     @Override
     public Product seleteById(Integer id) {
-        return productMapper.selectByPrimaryKey(id);
+        Product product = (Product) redisTemplate.opsForValue().get("product:" + id);
+        if (product == null) {
+            redissonClient.getLock("product_lock_" + id).lock();
+            try {
+                product = (Product) redisTemplate.opsForValue().get("product:" + id);
+                if (product == null) {
+                    product = productMapper.selectByPrimaryKey(id);
+                    if (product != null) {
+                        redisTemplate.opsForValue().set("product:" + id, product, 1, TimeUnit.DAYS);
+                    } else {
+                        redisTemplate.opsForValue().set("product:" + id, new Product(), 1, TimeUnit.MINUTES);
+                    }
+                }
+            } finally {
+                redissonClient.getLock("product_lock_" + id).unlock();
+            }
+        }
+        return product;
     }
+
+    /*@Override
+    public Product seleteById(Integer id) {
+        Product product = (Product) redisTemplate.opsForValue().get("product:" + id);
+        if (product == null) {
+            synchronized (this) {
+                product = (Product) redisTemplate.opsForValue().get("product:" + id);
+                if (product == null) {
+                    product = productMapper.selectByPrimaryKey(id);
+                    if (product != null) {
+                        redisTemplate.opsForValue().set("product:" + id, product, 1, TimeUnit.DAYS);
+                    } else {
+                        redisTemplate.opsForValue().set("product:" + id, new Product(), 1, TimeUnit.MINUTES);
+                    }
+                }
+            }
+        }
+        return product;
+    }*/
 }
